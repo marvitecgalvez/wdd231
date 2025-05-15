79023ff38976ebe567dc8fa69716e24b1e2e7bc1
marvitecgalvez <marvin.galvez.durand@gmail.com>
2025-05-14 22:28:46 -0500

    new

--- scripts/dates.js
+++ scripts/dates.js
@@ -22,6 +22,6 @@ const authorName = document.querySelector('#authorName');
 
 const myName = document.querySelector('#myName');
 
-myName = authorName.getAttribute('content');
+myName = authorName.getAttribute('content').valueOf;
 
 myName.innerHTML = document.contains;
