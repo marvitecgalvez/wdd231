e87259ba0a9df2324af9e5cb4a86cda97e37ff63
marvitecgalvez <marvin.galvez.durand@gmail.com>
2025-05-23 23:40:24 -0500

    new

--- chamber/scripts/cards.js
+++ chamber/scripts/cards.js
@@ -1,28 +1,28 @@
-const url = 'https://marvitecgalvez.github.io/wdd231/chamber/data/members.json';
+//const url = 'https://marvitecgalvez.github.io/wdd231/chamber/data/members.json';
 
 const cards = document.querySelector('#cards');
 
-async function getProphetData() {
-    const response = await fetch(url);
+async function getMemberData() {
+    const response = await fetch('data/members.json');
     const data = await response.json();
-    displayProphets(data.prophets);
+    displayMembers(data.members);
 }
 
-getProphetData();
+getMemberData();
 
-const displayProphets = (prophets) => {
-    prophets.forEach((prophets) => {
+const displayMembers = (members) => {
+    members.forEach((members) => {
         let card = document.createElement('section');
         let fullname = document.createElement('h2');
         let datebirth = document.createElement('p');
         let placebirth = document.createElement('p');
         let portrait = document.createElement('img');
 
-        fullname.textContent = `${prophets.name} ${prophets.lastname}`;
-        datebirth.innerHTML = `Date of Birth: ${prophets.birthdate}`;
-        placebirth.innerHTML = `Place of Birth: ${prophets.birthplace}`;
-        portrait.setAttribute('src', prophets.imageurl);
-        portrait.setAttribute('alt', `Portrait of ${prophets.name} ${prophets.lastname}`);
+        fullname.textContent = `${members.name} ${members.lastname}`;
+        datebirth.innerHTML = `Date of Birth: ${members.birthdate}`;
+        placebirth.innerHTML = `Place of Birth: ${members.birthplace}`;
+        portrait.setAttribute('src', members.imageurl);
+        portrait.setAttribute('alt', `Portrait of ${members.name} ${members.lastname}`);
         portrait.setAttribute('loading', 'lazy');
         portrait.setAttribute('width', '200');
         portrait.setAttribute('height', 'auto');
