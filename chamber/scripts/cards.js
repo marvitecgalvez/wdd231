8530fd47e44f8c21a71fd2751cbb670555aab931
marvitecgalvez <marvin.galvez.durand@gmail.com>
2025-05-24 00:42:11 -0500

    new

--- chamber/scripts/cards.js
+++ chamber/scripts/cards.js
@@ -22,7 +22,7 @@ const displayMembers = (members) => {
         let since = document.createElement('p');
 
 
-        logo.setAttribute('src', members.url);
+        logo.setAttribute('src', members.image);
         logo.setAttribute('alt', `logo of ${members.name}`);
         logo.setAttribute('loading', 'lazy');
         logo.setAttribute('width', '200');
@@ -30,16 +30,29 @@ const displayMembers = (members) => {
         name.textContent = `${members.name}`;
         address.innerHTML = `Address: ${members.address}`;
         phone.innerHTML = `Phone: ${members.phone}`;
-        web.textContent = `<a href="${members.url}">Visit Website</a>`;
-        level.innerHTML = `Membership: ${getMembership(members.level)}`;
-        since.innerHTML = `Member since: ${members.since}`;
+        web.innerHTML = `<a href="${members.url}">Visit Website</a>`;
+        //level.innerHTML = `Membership: ${getMembership(members.level)}`;
+        since.innerHTML = `Member since: ${members.year}`;
 
-
+        card.appendChild(logo);
         card.appendChild(name);
         card.appendChild(address);
         card.appendChild(phone);
-        card.appendChild(logo);
+        card.appendChild(web);
+        card.appendChild(level);
+        card.appendChild(since);
+
 
         cards.appendChild(card);
     });
+}
+
+function getMembership(level) {
+    let nivel = 'Member';
+    if (level.level === 3) {
+        nivel = 'Gold';
+    }
+    if (level.level === 2) {
+        nivel = 'Silver';
+    }
 }
