92085b8c6b1bc58fd0f65723ebedb8b238cab30d
marvitecgalvez <marvin.galvez.durand@gmail.com>
2025-05-28 00:25:01 -0500

    new

--- scripts/weather.mjs
+++ scripts/weather.mjs
@@ -4,23 +4,6 @@ const captionDesc = document.querySelector('figcaption');
 
 const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=-6.64&units=imperial&appid=d0271fe8ae8e6abe5134eb6cd30b5084';
 
-/*async function apiFetch() {
-    try {
-        const response = await fetch(url);
-        if (response.ok) {
-            const data = await response.json();
-            console.log(data);
-        } else {
-            throw Error(await response.text());
-        }
-    } catch (error) {
-        console.log(error);
-    }
-
-apiFetch();
-
-}*/
-
 async function apiFetch() {
     try {
         const response = await fetch(url);
